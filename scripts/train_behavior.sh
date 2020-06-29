afaed604264f868dd5446cf35ec9b9263456efae
ihaeyong <ihaeyong@gmail.com>
2020-06-29 03:06:51 +0000

    mend

--- scripts/train_behavior.sh
+++ scripts/train_behavior.sh
@@ -5,4 +5,4 @@ export PYTHONPATH=/root/workspaces/vtt/VTT_TRACKING/Face_recog
 export PYTHONIOENCODING=utf-8
 export CUDA_VISIBLE_DEVICES=$1
 
-python models/train_behavior.py -model global_reweight3_focal_one_gamma3_none_v1 -b_loss ce_focal -f_gamma 3
+python models/train_behavior.py -model global_diff_reweight_focal_one_gamma3_none_v1 -b_loss ce_focal -f_gamma 3
