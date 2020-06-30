61608aa4fac85acc9fb4ab6586042be864a72512
ihaeyong <ihaeyong@gmail.com>
2020-06-30 08:06:55 +0000

    mend

--- scripts/eval_behavior.sh
+++ scripts/eval_behavior.sh
@@ -4,4 +4,4 @@ export PYTHONIOENCODING=utf-8
 export CUDA_VISIBLE_DEVICES=$1
 
 
-python models/eval_behavior.py -model global_diff_reweight_focal_one_gamma3_batch
+python models/eval_behavior.py -model global_diff_reweight_focal_one_gamma3_batch_conv1d
