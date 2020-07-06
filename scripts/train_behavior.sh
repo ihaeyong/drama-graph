49b4883d1634fc561c998d8a21236b26c78b4b78
Hobin Ryu <hobincar@kaist.ac.kr>
2020-07-06 06:37:22 +0000

    Modify gitignore.

--- scripts/train_behavior.sh
+++ scripts/train_behavior.sh
@@ -3,6 +3,7 @@
 # Train Object Detection Model (YOLO_V2)
 export PYTHONPATH=/root/workspaces/vtt/VTT_TRACKING/Face_recog
 export PYTHONIOENCODING=utf-8
-export CUDA_VISIBLE_DEVICES=$1
+# export CUDA_VISIBLE_DEVICES=$1
 
-python models/train_behavior.py -model global_diff_reweight_focal_one_gamma2_conv1d_lowdim -b_loss ce_focal -f_gamma 2
+python -m models.train_behavior -model detector_behavior_conv1d_lowdim -b_loss ce 
+
