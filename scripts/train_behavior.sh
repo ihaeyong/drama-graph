be3d2da2865883726b9b849f4e8b525392d8dce4
ihaeyong <ihaeyong@gmail.com>
2020-07-08 06:15:38 +0000

    focal loss

--- scripts/train_behavior.sh
+++ scripts/train_behavior.sh
@@ -5,4 +5,4 @@ export PYTHONPATH=/root/workspaces/vtt/VTT_TRACKING/Face_recog
 export PYTHONIOENCODING=utf-8
 export CUDA_VISIBLE_DEVICES=$1
 
-python models/train_behavior.py -model global_diff_subset_batch_local -b_loss ce
+python models/train_behavior.py -model global_diff_subset_batch_local_focal -b_loss ce_focal
