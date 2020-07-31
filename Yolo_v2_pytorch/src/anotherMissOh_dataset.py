27b946eb78a634ef62d4e3adc5c252020755c59f
chojw <57617399+chojw@users.noreply.github.com>
2020-07-31 13:01:03 +0900

    Update anotherMissOh_dataset.py

--- Yolo_v2_pytorch/src/anotherMissOh_dataset.py
+++ Yolo_v2_pytorch/src/anotherMissOh_dataset.py
@@ -31,7 +31,7 @@ P2PRelCLS = ['Friendly', 'Unfriendly', 'none']
 
 # define object classes 
 
-# ObjectCLS = ["person", "apron", "tie", "bottle", "table", "chair(stool)", "vase", "couch", "remote_control", "potted_plant", 
+# ObjectCLS = ["apron", "tie", "bottle", "table", "chair(stool)", "vase", "couch", "remote_control", "potted_plant", 
 #             "glass", "handbag", "bag", "lamp", "food", "fork", "basket", "clock", "computer", "TV", "paper(report)", "glasses", 
 #             "binder", "can/beer", "plate", "phone", "flower_pot", "book", "frame", "car", "bed", "tree", "flower", "watch", 
 #             "wine_glass", "can", "suitcase", "bookshelf", "shoes", "box", "door", "cabinet", "desk", "umbrella", "laptop", "bowl", 
@@ -41,32 +41,22 @@ P2PRelCLS = ['Friendly', 'Unfriendly', 'none']
 #             "frying_pan", "purse", "towel", "plastic_bag", "hat", "bus", "microwave", "soccer_ball", "bench", "notebook", 
 #             "cigarette", "cakes", "backpack", "teddy_bear", "photo", "wine_bottle", "butterfly", "drawer", "sunglasses", "hammer", 
 #             "knife", "sandwitch", "carrot", "cushion", "tape_player", "sports_ball", "envelope", "washingmachine", "blender", 
-#             "pencil_box", "dish", "paper_towel", "lipstick", "ladle", "paper (report)", "camera", "baseball_bat", "kettle", "blanket", 
+#             "pencil_box", "dish", "paper_towel", "lipstick", "ladle", "paper_(report)", "camera", "baseball_bat", "kettle", "blanket", 
 #             "motorcycle", "candle", "filing_cabinet", "digital_clock", "brick", "bird", "coffeepot", "trumpet", "cat", "grass", "card", 
 #             "detergent_box", "board", "scissors", "star", "mouse", "broccoli", "building", "bat", "train", "nail", 
 #             "pencil_sharpener", "airplane", "poster", "guitar", "floor", "plate_rack", "accordion", "swine", "spatula", "rabbit", 
 #             "oven", "saxophone", "tennis_ball", "cow", "banana", "stop_sign", "boat", "cream", "tennis_racket", "orange", "baseball", 
 #             "skateboard", "perfume", "elephant", "axe", "hamburger"]
 
-ObjectCLS = ["apron", "tie", "bottle", "table", "chair(stool)", "vase", "couch", "remote_control", "potted_plant", 
-            "glass", "handbag", "bag", "lamp", "food", "fork", "basket", "clock", "computer", "TV", "paper(report)", "glasses", 
-            "binder", "can/beer", "plate", "phone", "flower_pot", "book", "frame", "car", "bed", "tree", "flower", "watch", 
-            "wine_glass", "can", "suitcase", "bookshelf", "shoes", "box", "door", "cabinet", "desk", "umbrella", "laptop", "bowl", 
-            "microphone", "fan", "refrigerator", "shelf", "beer_bottle", "apple", "pillow", "sofa", "window", "water_bottle", 
-            "coffee_maker", "keyboard", "plant", "tooth_brush", "machine", "bicycle", "cup_or_mug", "clothes", "cap", "bow_tie", 
-            "truck", "traffic_light", "stove", "chopsticks", "spoon", "pot", "sink", "racket", "dog", "horse", "remote", 
-            "frying_pan", "purse", "towel", "plastic_bag", "hat", "bus", "microwave", "soccer_ball", "bench", "notebook", 
-            "cigarette", "cakes", "backpack", "teddy_bear", "photo", "wine_bottle", "butterfly", "drawer", "sunglasses", "hammer", 
-            "knife", "sandwitch", "carrot", "cushion", "tape_player", "sports_ball", "envelope", "washingmachine", "blender", 
-            "pencil_box", "dish", "paper_towel", "lipstick", "ladle", "paper (report)", "camera", "baseball_bat", "kettle", "blanket", 
-            "motorcycle", "candle", "filing_cabinet", "digital_clock", "brick", "bird", "coffeepot", "trumpet", "cat", "grass", "card", 
-            "detergent_box", "board", "scissors", "star", "mouse", "broccoli", "building", "bat", "train", "nail", 
-            "pencil_sharpener", "airplane", "poster", "guitar", "floor", "plate_rack", "accordion", "swine", "spatula", "rabbit", 
-            "oven", "saxophone", "tennis_ball", "cow", "banana", "stop_sign", "boat", "cream", "tennis_racket", "orange", "baseball", 
-            "skateboard", "perfume", "elephant", "axe", "hamburger"]
+# this is what we decided to use because there is too many. 47 classes
+ObjectCLS = ["bag", "bed", "beer_bottle", "bicycle", "book", "bookshelf", "bottle", "bowl", "box", "bus", "can_beer", 
+            "car", "chair(stool)", "computer", "couch", "desk", "door", "flower", "food", "frame", "glass", "glasses", 
+            "handbag", "hat", "lamp", "laptop", "microphone", "paper(report)", "phone", "plant", "plate", "pot", 
+            "potted_plant", "refrigerator", "shoes", "sofa", "spoon", "table", "tie", "traffic_light", "tree", "TV", 
+            "umbrella", "vase", "watch", "window", "wine_glass"]
 
 
-P2ORelCLS = ['Holding', 'Wearing', 'none']
+P2ORelCLS = ['none', 'wearing', 'on', 'with', 'in front of', 'has', 'in', 'near', 'attached to', 'under', 'at', 'N_P', 'N_O']
 
 
 def Splits(num_episodes):
@@ -89,6 +79,7 @@ def SortFullRect(image,label, is_train=True):
     fullrect_list = []
     fullbehav_list = []
     fullobj_list = []
+    fullrelation_list = []
     num_batch = len(label[0]) # per 1 video clip
 
     # set sequence length
@@ -109,10 +100,10 @@ def SortFullRect(image,label, is_train=True):
         try:
             label_list = []
             behavior_list = []
+            frame_id = label[0][frm]['frame_id']
+            frame_id_list.append(frame_id)
             for p, p_id in enumerate(label[0][frm]['persons']['person_id']):
 
-                frame_id = label[0][frm]['frame_id']
-                frame_id_list.append(frame_id)
 
                 p_label = PersonCLS.index(p_id)
                 if p_label > 20:
@@ -140,12 +131,14 @@ def SortFullRect(image,label, is_train=True):
     for frm in range(s_frm, e_frm):
         try:
             object_list = []
+            # relation_list = []
             for p, p_id in enumerate(label[0][frm]['objects']['object_id']):
                 # we need to account for 'person' and remove person from the list
                 if p_id == 'person':
                     continue
                 p_label = ObjectCLS.index(p_id)
-                if p_label > 162:
+                r_label = P2ORelCLS.index(label[0][frm]['objects']['relation'][p])
+                if p_label > 47:
                     print("sort full rect index error{}".format(p_label))
                 # object labels
                 object_rect = label[0][frm]['objects']['object_rect'][p]
@@ -157,7 +150,9 @@ def SortFullRect(image,label, is_train=True):
                 ymax = min((object_rect[3]) * height_ratio, 448)
                 object_rect = [xmin,ymin,xmax,ymax]
 
-                temp_label = np.concatenate((object_rect, [p_label]), 0)
+                temp_label = np.concatenate((object_rect, [p_label], [r_label]), 0)
+                # temp_label = np.concatenate((object_rect, [p_label]), 0)
+                # relation_list.append(r_label)
                 object_list.append(temp_label)
         except:
             continue
@@ -167,11 +162,13 @@ def SortFullRect(image,label, is_train=True):
             fullbehav_list.append(behavior_list)
             image_list.append(image[frm])
             fullobj_list.append(object_list)
+            # fullrelation_list.append(relation_list)
         else: # for test
             fullrect_list.append(label_list)
             fullbehav_list.append(behavior_list)
             image_list.append(image[frm])
             fullobj_list.append(object_list)
+            # fullrelation_list.append(relation_list)
 
     if is_train:
         return image_list, fullrect_list, fullbehav_list, fullobj_list
@@ -291,19 +288,20 @@ class AnotherMissOh(Dataset):
                     image_info['objects']['object_rect']=[]
                     image_info['objects']['relation']=[]
                     for k, obj in enumerate(info['objects']):
-                        image_info['objects']['object_id'].append(obj['object_id'])
-                        object_bbox = obj['object_rect']
-                        if (object_bbox['min_y'] == "" 
-                            or object_bbox['max_y'] == "" 
-                            or object_bbox['min_x'] == "" 
-                            or object_bbox['max_x'] == ""):
-                            object_rect = []
-                            continue
-                        else:
-                            object_rect = [object_bbox['min_x'], object_bbox['min_y'], 
-                                           object_bbox['max_x'], object_bbox['max_y']]
+                        if obj['object_id'] in ObjectCLS:
+                            image_info['objects']['object_id'].append(obj['object_id'])
+                            object_bbox = obj['object_rect']
+                            if (object_bbox['min_y'] == "" 
+                                or object_bbox['max_y'] == "" 
+                                or object_bbox['min_x'] == "" 
+                                or object_bbox['max_x'] == ""):
+                                object_rect = []
+                                continue
+                            else:
+                                object_rect = [object_bbox['min_x'], object_bbox['min_y'], 
+                                               object_bbox['max_x'], object_bbox['max_y']]
                         image_info['objects']['object_rect'].append(object_rect)
-                        image_info['objects']['relation'].append('none')
+                        image_info['objects']['relation'].append('N_P')
                     # objects
 
                     image_info['persons']['person_id'] = []
@@ -343,7 +341,6 @@ class AnotherMissOh(Dataset):
                                 image_info['objects']['relation'].append('none')
                             else:
                                 image_info['objects']['relation'].append(person['person_info']['predicate'])
-
 
                         face_bbox = person['person_info']['face_rect']
                         if (face_bbox['min_y'] == "" 
