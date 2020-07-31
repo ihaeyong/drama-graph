3797277e0d6badfd5c9aaa1b427ae7f26a2fd930
chojw <57617399+chojw@users.noreply.github.com>
2020-07-31 12:59:23 +0900

    Update train_models.sh

--- scripts/train_models.sh
+++ scripts/train_models.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Train Object Detection Model (YOLO_V2)
-export PYTHONPATH=/root/workspaces/vtt/VTT_TRACKIng/Face_recog
+# export PYTHONPATH=/root/workspaces/vtt/VTT_TRACKIng/Face_recog
 export PYTHONIOENCODING=utf-8
-export CUDA_VISIBLE_DEVICES=$1
+export CUDA_VISIBLE_DEVICES=0
 
-python train_main.py -model logger
+python train_main.py -model obj47_person_relation
 
 
