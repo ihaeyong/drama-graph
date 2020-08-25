ac33cd3820ec149b6d8538f8179babb9374a533e
chojw <chojw2017@gmail.com>
2020-08-25 11:30:46 +0900

    edit scripts and add scripts to run object

--- scripts/train_relation.sh
+++ scripts/train_relation.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Train Object Detection Model (YOLO_V2)
-# export PYTHONPATH=/root/workspaces/vtt/VTT_TRACKIng/Face_recog
 export PYTHONIOENCODING=utf-8
 export CUDA_VISIBLE_DEVICES=0
 
