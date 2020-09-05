74464d6e6186eed49526ce90f3a4d665c263e4bc
ihaeyong <ihaeyong@gmail.com>
2020-09-05 07:40:16 +0000

    adjust lr for behaivor

--- models/train_behavior.py
+++ models/train_behavior.py
@@ -131,11 +131,11 @@ def train(opt):
     non_fc_params = [p for n,p in model.named_parameters()
                      if not n.startswith('detector') and p.requires_grad]
 
-    p_params = [{'params': fc_params, 'lr': opt.lr * 10.0}]
+    p_params = [{'params': fc_params, 'lr': opt.lr}]
     b_params = [{'params': non_fc_params, 'lr': opt.lr * 10.0}]
 
     criterion = YoloLoss(num_persons, model.detector.anchors, opt.reduction)
-    p_optimizer = torch.optim.SGD(p_params, lr = opt.lr * 10.0,
+    p_optimizer = torch.optim.SGD(p_params, lr = opt.lr,
                                   momentum=opt.momentum,
                                   weight_decay=opt.decay)
     b_optimizer = torch.optim.SGD(b_params, lr = opt.lr * 10.0,
