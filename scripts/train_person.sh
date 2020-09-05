1a0e1faac4031039251cf36e8a3181f032b85927
ihaeyong <ihaeyong@gmail.com>
2020-09-05 09:08:18 +0000

    add person model training

---
+++ scripts/train_person.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Train Object Detection Model (YOLO_V2)
+export PYTHONPATH=/root/workspaces/vtt/drama-graph
+export PYTHONIOENCODING=utf-8
+export CUDA_VISIBLE_DEVICES=$1
+
+python models/train_person.py -model voc_person
