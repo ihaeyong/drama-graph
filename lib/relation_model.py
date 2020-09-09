0bb0738e9982f2a45ee9def19eeead182b2f8279
chojw <chojw2017@gmail.com>
2020-09-09 16:33:22 +0900

    update relation model

--- lib/relation_model.py
+++ lib/relation_model.py
@@ -1,3 +1,6 @@
+"""
+@author: Haeyong Kang
+"""
 import torch.nn as nn
 import torch
 
@@ -11,30 +14,200 @@ from Yolo_v2_pytorch.src.yolo_net import Yolo
 from Yolo_v2_pytorch.src.yolo_tunning import YoloD
 
 from Yolo_v2_pytorch.src.rois_utils import anchorboxes
-from Yolo_v2_pytorch.src.anotherMissOh_dataset import ObjectCLS, P2ORelCLS
+from Yolo_v2_pytorch.src.anotherMissOh_dataset import PersonCLS, ObjectCLS, P2ORelCLS
+
+from lib.person_model import person_model
+from lib.object_model import object_model
 
 import numpy as np
+import pdb
 
 class relation_model(nn.Module):
-    def __init__(self, num_objects, num_relations):
+    def __init__(self, num_persons, num_objects, num_relations, opt, device):
         super(relation_model, self).__init__()
 
-        num_persons=20
-        pre_model = Yolo(num_persons)
-        self.detector = YoloD(pre_model)
+        # just for reference (anchor information)
+        num_objects = 47
+        num_relations = 13
+        num_persons = 20
 
-        self.num_objects = num_objects
+        self.person_model = person_model(num_persons, device)
+        self.object_model = object_model(num_objects).cuda(device)
+        self.person_detector = self.person_model.detector
+        self.object_detector = self.object_model.detector
+
+        self.num_persons = num_persons
         self.num_relations = num_relations
+        self.num_objects = num_objects
 
-        self.conv = nn.Conv2d(
-            1024, len(self.detector.anchors) * (5 + num_objects + num_relations), 1, 1, 0, bias=False)
+        # define convs
+        self.person_conv = nn.Sequential(
+            nn.Conv2d(1024, 512, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 256, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+
+        self.object_conv = nn.Sequential(
+            nn.Conv2d(1024, 512, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 256, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
 
 
-    def forward(self, input):
-        # run the model through the main detector
-        output, output_1 = self.detector(input)
+        self.relation_conv1d_person = nn.Sequential(
+            nn.Conv1d(2304, 2304, 3, stride=1, padding=1),
+            nn.ReLU(),nn.Dropout(0.1),
+            nn.Conv1d(2304, 2304, 3, stride=1, padding=1),
+            nn.ReLU(),nn.Dropout(0.1),
+        )
 
-        # run each individual model
-        output = self.conv(output)
+        self.relation_conv1d_object = nn.Sequential(
+            nn.Conv1d(2304, 2304, 3, stride=1, padding=1),
+            nn.ReLU(),nn.Dropout(0.1),
+            nn.Conv1d(2304, 2304, 3, stride=1, padding=1),
+            nn.ReLU(),nn.Dropout(0.1),
+        )
 
-        return output, output_1
+        self.relation_fc = nn.Sequential(
+            nn.Linear(256 * 3 * 3 * 2, 1024),
+            nn.ReLU(),nn.Dropout(0.1),
+            nn.Linear(1024, num_relations))
+
+        self.img_size = opt.image_size
+        self.conf_threshold = opt.conf_threshold
+        self.nms_threshold = opt.nms_threshold
+        self.device=device
+
+        self.gt_boxes = True
+
+    def ex_global_feat_person(self, fmap):
+        box_g = torch.from_numpy(
+            np.array([0,0,self.fmap_size,self.fmap_size])).cuda(
+                self.device).detach()
+        g_box = Variable(
+            torch.zeros(1, 5).cuda(self.device)).detach()
+        g_box[:,1:] = box_g
+
+        g_fmap = roi_align(fmap[None],
+                           g_box.float(),
+                           (self.fmap_size//4,
+                            self.fmap_size//4))
+
+        g_fmap = self.person_conv(g_fmap)
+
+        return g_fmap
+
+    def ex_global_feat_object(self, fmap):
+        box_g = torch.from_numpy(
+            np.array([0,0,self.fmap_size,self.fmap_size])).cuda(
+                self.device).detach()
+        g_box = Variable(
+            torch.zeros(1, 5).cuda(self.device)).detach()
+        g_box[:,1:] = box_g
+
+        g_fmap = roi_align(fmap[None],
+                           g_box.float(),
+                           (self.fmap_size//4,
+                            self.fmap_size//4))
+
+        g_fmap = self.object_conv(g_fmap)
+
+        return g_fmap
+
+
+    def forward(self, image, label, object_label):
+
+        # person detector
+        logits, fmap = self.person_model(image)
+        object_logits, obj_fmap = self.object_model(image)
+        batch = logits.size(0)
+
+        fmap = fmap.detach()
+        obj_fmap = obj_fmap.detach()
+
+        # fmap [b, 1024, 14, 14]
+        self.fmap_size = fmap.size(2)
+        self.obj_fmap_size = obj_fmap.size(2)
+
+
+        # persons boxes
+        r_logits = []
+        r_labels = []
+
+        # training
+        if len(label) > 0 and self.training:
+            if len(object_label) > 0:
+                for idx, box in enumerate(label):
+                    num_box = len(box)
+                    g_fmap = self.ex_global_feat_person(fmap[idx])
+
+                    if num_box == 0 :
+                        continue
+
+                    box_ = np.clip(
+                        np.stack(box)[:,:4].astype('float32')/self.img_size,
+                        0.0, self.fmap_size) * self.fmap_size
+                    box_ = torch.from_numpy(box_).cuda(self.device).detach()
+                    b_box = Variable(
+                        torch.zeros(num_box, 5).cuda(self.device)).detach()
+                    b_box[:,1:] = torch.clamp(box_ + torch.randn(box_.shape).cuda(
+                        self.device), 0, self.fmap_size)
+                    i_fmap = roi_align(fmap[idx][None],
+                                       b_box.float(),
+                                       (self.fmap_size//4,
+                                        self.fmap_size//4))
+
+                    # local feature
+                    i_fmap = self.person_conv(i_fmap)
+
+                    i_fmap += g_fmap
+                    rr_labels = []
+                    rr_logits = []
+
+                    for jdx, obj_box in enumerate(object_label[idx]):
+                        obj_num_box = 1
+
+                        try:
+                            obj_g_fmap = self.ex_global_feat_object(obj_fmap[jdx])
+                        except:
+                            pdb.set_trace()
+                        if len(obj_box) == 0 :
+                            continue
+
+                        obj_box = [obj_box]
+                        obj_box_ = np.clip(
+                            np.stack(obj_box)[:,:4].astype('float32')/self.img_size,
+                            0.0, self.obj_fmap_size) * self.obj_fmap_size
+                        obj_box_ = torch.from_numpy(obj_box_).cuda(self.device).detach()
+                        obj_b_box = Variable(
+                            torch.zeros(obj_num_box, 5).cuda(self.device)).detach()
+                        obj_b_box[:,1:] = torch.clamp(obj_box_ + torch.randn(obj_box_.shape).cuda(
+                            self.device), 0, self.obj_fmap_size)
+                        obj_i_fmap = roi_align(obj_fmap[idx][None],
+                                           obj_b_box.float(),
+                                           (self.obj_fmap_size//4,
+                                            self.obj_fmap_size//4))
+
+                        
+                        obj_i_fmap = self.object_conv(obj_i_fmap)
+
+                        rr_labels.append(int(obj_box[0][5]))
+                        if i_fmap.size(0)>1:
+                            pdb.set_trace()
+
+                        p_feat = self.relation_conv1d_person(i_fmap.view(1,-1).unsqueeze(2))[0]
+                        o_feat = self.relation_conv1d_object(obj_i_fmap.view(1,-1).unsqueeze(2))[0]
+
+                        r_feat = torch.cat((p_feat, o_feat),0).transpose(1,0)
+                        r_logit = self.relation_fc(r_feat)
+
+                        rr_logits.append(r_logit)
+
+                    r_logits.append(rr_logits)
+                    r_labels.append(rr_labels)
+
+        return r_logits, r_labels
--- models/train_relation.py
+++ models/train_relation.py
@@ -1,24 +1,24 @@
 import os
 import argparse
+import torch
 import torch.nn as nn
+from torch.nn import functional as F
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from Yolo_v2_pytorch.src.anotherMissOh_dataset import AnotherMissOh, Splits, SortFullRect
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from Yolo_v2_pytorch.src.anotherMissOh_dataset import AnotherMissOh, Splits, SortFullRect, PersonCLS, ObjectCLS, P2ORelCLS
 from Yolo_v2_pytorch.src.utils import *
 from Yolo_v2_pytorch.src.loss import YoloLoss
-from Yolo_v2_pytorch.src.relation_loss import Relation_YoloLoss
-from Yolo_v2_pytorch.src.yolo_net import Yolo
-from Yolo_v2_pytorch.src.yolo_tunning import YoloD
 import shutil
-import visdom
 import cv2
 import pickle
 import numpy as np
 from lib.logger import Logger
-from tqdm import tqdm
-from collections import Counter
 
 from lib.relation_model import relation_model
-
+from lib.pytorch_misc import optimistic_restore, de_chunkize, clip_grad_norm, flatten
+from lib.focal_loss import FocalLossWithOneHot, FocalLossWithOutOneHot, CELossWithOutOneHot
+import pdb
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -33,7 +33,7 @@ def get_args():
     parser.add_argument("--momentum", type=float, default=0.9)
     parser.add_argument("--decay", type=float, default=0.0005)
     parser.add_argument("--dropout", type=float, default=0.5)
-    parser.add_argument("--num_epoches", type=int, default=50)
+    parser.add_argument("--num_epoches", type=int, default=100)
     parser.add_argument("--test_interval", type=int, default=1,
                         help="Number of epoches between testing phases")
     parser.add_argument("--object_scale", type=float, default=1.0)
@@ -49,8 +49,8 @@ def get_args():
     parser.add_argument("--pre_trained_model_type",
                         type=str, choices=["model", "params"],
                         default="model")
-    parser.add_argument("--pre_trained_model_path", type=str,
-                        default="Yolo_v2_pytorch/trained_models/only_params_trained_yolo_voc") # Pre-training path
+    parser.add_argument("--trained_model_path", type=str,
+                        default="./checkpoint") # Pre-training path
 
     parser.add_argument("--saved_path", type=str,
                         default="./checkpoint") # saved training path
@@ -62,6 +62,12 @@ def get_args():
     parser.add_argument("--json_path", type=str,
                         default="./data/AnotherMissOh/AnotherMissOh_Visual_ver3.2/")
     parser.add_argument("-model", dest='model', type=str, default="relation")
+    parser.add_argument("-lr", dest='lr', type=float, default=1e-5)
+    parser.add_argument("-clip", dest='clip', type=float, default=10.0)
+    parser.add_argument("-print_interval", dest='print_interval', type=int,
+                        default=100)
+    parser.add_argument("-f_gamma", dest='f_gamma', type=float, default=1.0)
+    parser.add_argument("-clip_grad", dest='clip_grad',action='store_true')
 
     args = parser.parse_args()
     return args
@@ -78,6 +84,9 @@ train_set = AnotherMissOh(train, opt.img_path, opt.json_path, False)
 val_set = AnotherMissOh(val, opt.img_path, opt.json_path, False)
 test_set = AnotherMissOh(test, opt.img_path, opt.json_path, False)
 
+num_persons = len(PersonCLS)
+num_objects = len(ObjectCLS)
+num_relations = len(P2ORelCLS)
 
 # logger path
 logger_path = 'logs/{}'.format(opt.model)
@@ -88,17 +97,12 @@ else:
     print('mkdir_{}'.format(logger_path))
 logger = Logger(logger_path)
 
-num_objects = 47
-num_relations = 13
-
 def train(opt):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(123)
         device = torch.cuda.current_device()
     else:
         torch.manual_seed(123)
-    learning_rate_schedule = {"0": 1e-5, "5": 1e-4,
-                              "80": 1e-5, "110": 1e-6}
 
     training_params = {"batch_size": opt.batch_size,
                        "shuffle": True,
@@ -111,93 +115,175 @@ def train(opt):
                    "collate_fn": custom_collate_fn}
 
     train_loader = DataLoader(train_set, **training_params)
-    test_loader = DataLoader(test_set, **training_params)
 
-    # load the model
+    # define behavior-model
+    model = relation_model(num_persons, num_objects, num_relations, opt, device)
+    trained_persons = opt.trained_model_path + os.sep + "{}".format(
+        'anotherMissOh_only_params_person.pth')
+    trained_objects = opt.trained_model_path + os.sep + "{}".format(
+        'anotherMissOh_only_params_object.pth')
 
-    model = relation_model(num_objects, num_relations).cuda(device)
+    ckpt_person = torch.load(trained_persons)
+    ckpt_object = torch.load(trained_objects)
 
-    nn.init.normal_(list(model.modules())[-1].weight, 0, 0.01)
+    if optimistic_restore(model.person_detector, ckpt_person):
+        print("loaded pre-trained person detector sucessfully.")
 
-    criterion = Relation_YoloLoss(num_objects, num_relations, model.detector.anchors, opt.reduction)
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-5,
-                                momentum=opt.momentum, weight_decay=opt.decay)
+    if optimistic_restore(model.object_detector, ckpt_object):
+        print("loaded pre-trained object detector sucessfully.")
+    model.cuda(device)
+
+    # get optim
+    # yolo detector and person
+    fc_params = [p for n,p in model.named_parameters()
+                 if n.startswith('detector') \
+                 or n.startswith('person') \
+                 or n.startswith('object')
+                 and p.requires_grad]
+    # behavior model
+    non_fc_params = [p for n,p in model.named_parameters()
+                     if not n.startswith('detector') \
+                     or not n.startswith('person')\
+                     or not n.startswith('object')
+                     and p.requires_grad]
+
+    p_params = [{'params': fc_params, 'lr': opt.lr}]
+    b_params = [{'params': non_fc_params, 'lr': opt.lr * 10.0}]
+
+    p_optimizer = torch.optim.SGD(p_params, lr = opt.lr,
+                                  momentum=opt.momentum,
+                                  weight_decay=opt.decay)
+    b_optimizer = torch.optim.SGD(b_params, lr = opt.lr * 10.0,
+                                  momentum=opt.momentum,
+                                  weight_decay=opt.decay)
+
+    p_scheduler = ReduceLROnPlateau(p_optimizer, 'min', patience=3,
+                                    factor=0.1, verbose=True,
+                                    threshold=0.0001, threshold_mode='abs',
+                                    cooldown=1)
+    b_scheduler = ReduceLROnPlateau(b_optimizer, 'min', patience=3,
+                                    factor=0.1, verbose=True,
+                                    threshold=0.0001, threshold_mode='abs',
+                                    cooldown=1)
 
     model.train()
     num_iter_per_epoch = len(train_loader)
 
+    loss_step = 0
+    criterion = nn.CrossEntropyLoss()
+
+
     for epoch in range(opt.num_epoches):
-        loss_step = 0
-        empty_object = 0
-        if str(epoch) in learning_rate_schedule.keys():
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = learning_rate_schedule[str(epoch)]
+        r_logit_list = []
+        r_label_list = []
+        r_loss_list = []
         for iter, batch in enumerate(train_loader):
 
             image, info = batch
 
             # sort label info on fullrect
-            image, _, _, object_label, _, _ = SortFullRect(image, info)
+            image, label, behavior_label, obj_label, face_label, emo_label = SortFullRect(
+                image, info, is_train=True)
+
+            if np.array(label).size == 0 :
+                print("iter:{}_person bboxs are empty".format(
+                    iter, label))
+                continue
+
+            if np.array(obj_label).size == 0 : 
+                print("iter:{}_object bboxes are empty".format(
+                    iter, obj_label))
+                continue
 
             # image [b, 3, 448, 448]
-            try:
-                if torch.cuda.is_available():
-                    image = torch.cat(image).cuda()
-                else:
-                    image = torch.cat(image)
-            except:
-                continue
-
-            optimizer.zero_grad()
+            if torch.cuda.is_available():
+                image = torch.cat(image).cuda(device)
+            else:
+                image = torch.cat(image)
+                
+            p_optimizer.zero_grad()
+            b_optimizer.zero_grad()
 
             # logits [b, 125, 14, 14]
-            logits, _ = model(image)
-            device = logits.get_device()
+            r_logits, r_labels = model(image, label, obj_label)
 
-            # losses for object detection
-            if np.array(object_label).size != 0:
-                loss, loss_coord, loss_conf, loss_cls, loss_rel = criterion(logits, object_label, device)
-            else:
-                print("iter:{} object bboxs are empty".format(
-                    iter, object_label))
-                empty_object+=1
-                loss = torch.tensor(0, dtype=torch.float).cuda(device)
-                loss_coord = torch.tensor(0, dtype=torch.float).cuda(device)
-                loss_conf = torch.tensor(0, dtype=torch.float).cuda(device)
-                loss_cls = torch.tensor(0, dtype=torch.float).cuda(device)
-                continue
+
+            # loss for relation
+
+            loss = 0.0
+            for idx, rr_logits in enumerate(r_logits):
+                rr_logit_list = []
+                rr_label_list = []
+                rr_labels = r_labels[idx]
+                if len(rr_logits) == 0:
+                    continue
+                rr_logits = torch.stack(rr_logits).squeeze(1)
+
+                rr_labels = np.array(flatten(rr_labels))
+                #b_labels = np.stack(b_labels)
+
+
+                rr_labels = Variable(
+                    torch.LongTensor(rr_labels).cuda(device),
+                    requires_grad=False)
+                print('relation_label:{}'.format(rr_labels))
+
+                rr_label_list.append(rr_labels)
+                rr_logit_list.append(rr_logits)
+
+
+                rr_logits = torch.cat(rr_logit_list, 0)
+                rr_labels = torch.cat(rr_label_list, 0)
+                loss_relation = criterion(rr_logits, rr_labels)
+
+                loss += loss_relation
+
 
             loss.backward()
-            optimizer.step()
 
-            print("Epoch: {}/{}, Iteration: {}/{}, lr:{}".format(
+            if opt.clip_grad:
+                clip_grad_norm(
+                    [(n, p) for n, p in model.named_parameters()
+                     if p.grad is not None and not n.startswith('detector')],
+                    max_norm=opt.clip, verbose=verbose, clip=True)
+            b_optimizer.step()
+
+            print("Model:{}".format(opt.model))
+            print("Epoch: {}/{}, Iteration: {}/{}, lr:{:.9f}".format(
                 epoch + 1, opt.num_epoches,iter + 1,
-                num_iter_per_epoch, optimizer.param_groups[0]['lr']))
-            print("+loss:{:.2f}(coord:{:.2f},conf:{:.2f},cls:{:.2f})".format(
-                loss, loss_coord, loss_conf, loss_cls))
+                num_iter_per_epoch, p_optimizer.param_groups[0]['lr']))
+            print("+lr:{:.9f}, cls_relation:{:.2f}".format(
+                b_optimizer.param_groups[0]['lr'],
+                loss))
+            print()
 
-            loss_dict = {
-                'total' : loss.item(),
-                'coord' : loss_coord.item(),
-                'conf' : loss_conf.item(),
-                'cls' : loss_cls.item()
-            }
+            loss_dict = {}
+
+            loss_dict['cls_relation'] = loss.item()
 
             # Log scalar values
             for tag, value in loss_dict.items():
                 logger.scalar_summary(tag, value, loss_step)
 
             loss_step = loss_step + 1
+            r_loss_list.append(loss.item())
 
         print("SAVE MODEL")
+        if not os.path.exists(opt.saved_path):
+            os.makedirs(opt.saved_path + os.sep + "{}".format(opt_model))
+            print('mkdir_{}'.format(opt.saved_path))
+
+        # learning rate schedular
+        r_loss_avg = np.stack(r_loss_list).mean()
+
+        b_scheduler.step(r_loss_avg)
+
         torch.save(model.state_dict(),
-                   opt.saved_path + os.sep + "anotherMissOh_only_params_{}.pth".format(opt.model))
+                   opt.saved_path + os.sep + "anotherMissOh_only_params_{}.pth".format(
+                       opt.model))
         torch.save(model,
                    opt.saved_path + os.sep + "anotherMissOh_{}.pth".format(
                        opt.model))
-        print("Total number of missing objects: %d" % empty_object)
-        print("Total number of images: %d" % iter)
-        
 
 if __name__ == "__main__":
     train(opt)
