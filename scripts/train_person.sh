1af766e0d5ddd070c128140f27beb6056d1728f2
ihaeyong <ihaeyong@gmail.com>
2020-10-14 04:36:09 +0000

    included none

--- scripts/train_person.sh
+++ scripts/train_person.sh
@@ -3,6 +3,6 @@
 # Train Object Detection Model (YOLO_V2)
 export PYTHONPATH=$HOME/workspace/drama-graph
 export PYTHONIOENCODING=utf-8
-export CUDA_VISIBLE_DEVICES=$1,$2,$3
+export CUDA_VISIBLE_DEVICES=$1,$2
 
-python models/train_person.py -model voc_person_group_3gpu_init
+python models/train_person.py -model voc_person_group_2gpu_init_none
