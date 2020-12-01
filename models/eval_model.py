5128efc6b40cb2808059c13a3cae2a80a3d4831b
ihaeyong <ihaeyong@gmail.com>
2020-12-01 12:38:37 +0000

    need to update face, behavior, relation

--- models/eval_model.py
+++ models/eval_model.py
@@ -87,7 +87,7 @@ transf = Compose(tform)
 train, val, test = Splits(num_episodes=18)
 
 # load datasets
-episode = 6 # 7,8 checked 
+episode = 10 # 7,8 checked 
 infer = [episode]
 infer_set = AnotherMissOh(infer, opt.img_path, opt.json_path, False)
 
